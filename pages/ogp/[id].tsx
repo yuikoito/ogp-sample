5c3b7951492927075f4cc1705d81d710d7b5604b
YuikoIto <toffywing@gmail.com>
2021-06-26 23:16:27 +0900

    feat::tada: add ogp template

--- pages/ogp/[id].tsx
+++ pages/ogp/[id].tsx
@@ -1,10 +1,38 @@
+import React from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
-const Post = () => {
+const Page = () => {
   const router = useRouter();
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "";
   const { id } = router.query;
 
-  return <p>投稿: {id}</p>;
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          key="ogImage"
+          content={`${baseUrl}/ogp/${id}.png`}
+        />
+        <meta
+          name="twitter:card"
+          key="twitterCard"
+          content="summary_large_image"
+        />
+        <meta
+          name="twitter:image"
+          key="twitterImage"
+          content={`${baseUrl}/ogp/${id}.png`}
+        />
+      </Head>
+      <div>
+        <h1>{id}のページだよ</h1>
+      </div>
+    </>
+  );
 };
 
-export default Post;
+export default Page;
